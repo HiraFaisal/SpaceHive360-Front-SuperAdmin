dbbb6b0888fbf837f1225467d76246dfb335e870
HamzaFaisal28 <hamza.faisal7676@gmail.com>
2026-05-06 14:20:42 +0500

    updated proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://168.144.125.16:5003/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
